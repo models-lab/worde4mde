4b8805604df44c3f4b75c1ca1e00697935e0685f
Jesús Sánchez Cuadrado <jesusc@um.es>
2023-03-30 23:24:30 +0200

    Allow external hosts

--- web-playground/playground/settings.py
+++ web-playground/playground/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
